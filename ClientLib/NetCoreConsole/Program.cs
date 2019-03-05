1a1476a2d8b59be12f019a88a0915178a542be5e
Curt Sturm <csturm83@users.noreply.github.com>
2019-03-04 21:03:01 -0600

    dotnet/wcf Issue #3408 repro.

---
+++ ClientLib/NetCoreConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ClientLib;
+
+namespace NetCoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Service1Client client = new Service1Client())
+            {
+                var response = client.GetDataAsync(32769).Result; // MaxStringContentLength = 32768
+            }
+            Console.WriteLine(@"Made it without exception.");
+            Console.ReadLine();
+        }
+    }
+}
